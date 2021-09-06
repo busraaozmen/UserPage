8a3d77a24b26715da57c74df7060d1a327d058eb
Büşra ÖZMEN <busra.ozmenn@outlook.com>
2021-09-06 09:56:07 +0300

    Boş proje oluşturup gerekli kütüphaneleri ve paketleri oluşturdum.Startup Düzenledim.

--- UserPage/Startup.cs
+++ UserPage/Startup.cs
@@ -16,6 +16,10 @@ namespace UserPage
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddRazorRuntimeCompilation();
+            services.AddMvc(x => x.EnableEndpointRouting = false);
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +31,12 @@ namespace UserPage
             }
 
             app.UseRouting();
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
 
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
